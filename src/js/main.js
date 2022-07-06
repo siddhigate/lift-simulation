a1475e57e94f60489a61492961ea356d0dab1926
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2022-07-06 23:37:51 +0530

    feat: multiple lift requests

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,86 @@
 const buttons = document.querySelectorAll(".call-lift-btn");
-const lift = document.querySelector(".lift-container");
+const liftEls = document.querySelectorAll(".lift-container");
 const leftDoor = document.querySelector(".left-door");
 const rightDoor = document.querySelector(".right-door");
 
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  enqueue(element) {
+    return this.items.push(element);
+  }
+
+  dequeue() {
+    if (this.items.length > 0) {
+      return this.items.shift();
+    }
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length == 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  clear() {
+    this.items = [];
+  }
+}
+
+/**
+ *
+ *    LIFT Management
+ *    - get lifts
+ *    - get empty lift
+ *    - add lift
+ *
+ */
+
+const lifts = Array.from(
+  document.querySelectorAll(".lift-container"),
+  (el) => ({ htmlEl: el, busy: false })
+);
+
+function getLifts() {
+  return lifts;
+}
+
+function getEmptyLift() {
+  const lifts = getLifts();
+  const index = lifts.findIndex((lift) => lift.busy === false);
+
+  return { lift: lifts[index], index };
+}
+
+const getMaxLifts = () => {
+  const viewportwidth = document.getElementsByTagName("body")[0].clientWidth;
+};
+
+const callLift = () => {
+  const { lift, index } = getEmptyLift();
+
+  if (index >= 0 && !requests.isEmpty()) {
+    lifts[index].busy = true;
+    moveLift(lift.htmlEl, requests.dequeue(), index);
+  }
+};
+
+function addLift() {}
+
+/**
+ *
+ *    LIFT ACTIONS -> animations
+ *    open, close, move up, down.
+ *
+ */
 const openLift = () => {
   buttons.disabled = true;
   rightDoor.classList.add("right-door-open");
@@ -12,47 +90,100 @@ const openLift = () => {
   leftDoor.classList.remove("left-door-close");
 };
 
-const closeLift = () => {
+const closeLift = (index) => {
   rightDoor.classList.add("right-door-close");
   leftDoor.classList.add("left-door-close");
 
   rightDoor.classList.remove("right-door-open");
   leftDoor.classList.remove("left-door-open");
   buttons.disabled = false;
+
+  setTimeout(() => {
+    lifts[index].busy = false;
+    dispatchliftIdle();
+  }, 2500);
 };
 
-const openCloseLift = () => {
+const openCloseLift = (index) => {
   openLift();
   setTimeout(() => {
-    closeLift();
+    closeLift(index);
   }, 3000);
 };
 
-const moveLift = (distance, destFloor) => {
+const moveLift = (lift, destFloor, index) => {
+  const distance = Math.abs(destFloor - currFloor);
+
   lift.style.transform = `translateY(${destFloor * 100 * -1}%)`;
   lift.style.transition = `transform ${1500 * distance}ms ease-in-out`;
-};
-
-let currFloor = 0;
-
-function callLift(destFloor) {
-  const distance = Math.abs(destFloor - currFloor);
-  moveLift(distance, destFloor);
 
   setTimeout(() => {
-    openCloseLift();
+    openCloseLift(index);
   }, distance * 1500 + 1000);
 
   currFloor = destFloor;
-}
-
-for (let i = 0; i < buttons.length; i++) {
-  console.log(buttons[i].dataset.liftNum)
-  buttons[i].addEventListener("click", () => {
-    callLift(buttons[i].dataset.liftNum);
-  });
-}
-
-const getMaxLifts = () => {
-  const viewportwidth = document.getElementsByTagName("body")[0].clientWidth;
 };
+
+
+/**
+ *
+ *    REQUESTS:
+ *    addRequest
+ *    removeRequest
+ *
+ */
+
+function addRequest(destFloor) {
+  requests.enqueue(destFloor);
+  dispatchRequestAdded();
+}
+
+function removeRequest() {
+  requests.dequeue();
+}
+
+/**
+ *
+ *      EVENTS:
+ *      requestAddedEvent
+ *      liftIdleEvent
+ *
+ */
+
+const requestAddedEvent = new Event("requestAdded");
+const liftIdleEvent = new Event("liftIdle");
+
+function dispatchRequestAdded() {
+  document.dispatchEvent(requestAddedEvent);
+}
+
+function dispatchliftIdle() {
+  document.dispatchEvent(liftIdleEvent);
+}
+
+document.addEventListener("requestAdded", () => {
+  callLift();
+});
+
+document.addEventListener("liftIdle", () => {
+  callLift();
+});
+
+/**
+ *
+ * MAIN
+ *
+ */
+
+let currFloor = 0;
+let requests = new Queue();
+
+function main() {
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", () => {
+      addRequest(buttons[i].dataset.liftNum);
+    });
+  }
+}
+
+main();
